27a345332305a9d09eb6880261fcdeb0310a3655
tonvokhanhphuong <147031384+tonvokhanhphuong@users.noreply.github.com>
2023-10-12 07:20:47 +0000

    nopbai

--- s00_bailam.py
+++ s00_bailam.py
@@ -30,6 +30,15 @@ tongtienmuahang(None)                   | None
 #endregion debai
 
 #region bailam
+
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+    if gia_truoc_thue is None:
+        return None
+    else:
+        gia_sau_thue = gia_truoc_thue * 1.1
+        return int(gia_sau_thue)
+
+print(tongtienmuahang(1000000))
+print(tongtienmuahang(gia_truoc_thue=1000000))
+print(tongtienmuahang(None))
 #endregion bailam
